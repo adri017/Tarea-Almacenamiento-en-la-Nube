19b24cbd1149216e352da0ae873b9b6c3616eb69
adri017 <adrian.monge.gonzalez.al@iespoligonosur.org>
2026-01-21 20:40:49 +0100

    cambios en el script efs

--- efs.py
+++ efs.py
@@ -9,77 +9,58 @@ session = boto3.session.Session(
     aws_access_key_id=os.getenv("ACCESS_KEY"),
     aws_secret_access_key=os.getenv("SECRET_KEY"),
     aws_session_token=os.getenv("SESSION_TOKEN"),
-    region_name=os.getenv("REGION"))
+    region_name=os.getenv("REGION")
+)
 
-ec2 = session.client('ec2')
-efs = session.client('efs')
+ec2 = session.client("ec2")
+efs = session.client("efs")
 
 clave = "/home/alumnadotarde/Documentos/claves/claves.pem"
 txt = "prueba.txt"
-security_group = "sg-0d21f2e25aa648b88"
-subnet_id = "subnet-079a42116ab27f89e"
-
+security_group_ec2 = "sg-0d21f2e25aa648b88"
+security_group_efs = "sg-0d21f2e25aa648b88"
+subnet_id = "subnet-037805d4f9f153e2e"
 instancia = ec2.run_instances(
-                                ImageId="ami-07ff62358b87c7116",
-                                InstanceType="t3.micro",
-                                MinCount=1,
-                                MaxCount=1,
-                                KeyName="claves",
-                                SecurityGroupIds=[security_group],
-                                Placement={'AvailabilityZone': 'us-east-1a'},
-                                TagSpecifications=[{
-                                    'ResourceType': 'instance',
-                                    'Tags': [{'Key': 'Name','Value': 'ec2-efs'},]
-                                }],
-                            )
-instance_id = instancia['Instances'][0]['InstanceId']
-ec2.get_waiter('instance_running').wait(InstanceIds=[instance_id])
+    ImageId="ami-07ff62358b87c7116",
+    InstanceType="t3.micro",
+    MinCount=1,
+    MaxCount=1,
+    KeyName="claves",
+    SecurityGroupIds=[security_group_ec2]
+)
+instance_id = instancia["Instances"][0]["InstanceId"]
+ec2.get_waiter("instance_running").wait(InstanceIds=[instance_id])
 
 desc = ec2.describe_instances(InstanceIds=[instance_id])
-public_ip = desc['Reservations'][0]['Instances'][0]['PublicIpAddress']
+public_ip = desc["Reservations"][0]["Instances"][0]["PublicIpAddress"]
 
-print("EC2 creada:", instance_id)
+efs_response = efs.create_file_system(PerformanceMode="generalPurpose", Encrypted=False)
+efs_id = efs_response["FileSystemId"]
 
-efs_response = efs.create_file_system(
-    PerformanceMode='generalPurpose',
-    Encrypted=False,
-    Tags=[{'Key': 'Name', 'Value': 'EFS-Prueba'}]
-)
+while True:
+    fs = efs.describe_file_systems(FileSystemId=efs_id)
+    if fs["FileSystems"][0]["LifeCycleState"] == "available":
+        break
+    time.sleep(5)
 
-efs_id = efs_response['FileSystemId']
-print("EFS creado:", efs_id)
+efs.create_mount_target(FileSystemId=efs_id, SubnetId=subnet_id, SecurityGroups=[security_group_efs])
 
-time.sleep(10)
+while True:
+    mts = efs.describe_mount_targets(FileSystemId=efs_id)["MountTargets"]
+    if mts and all(mt["LifeCycleState"] == "available" for mt in mts):
+        break
+    time.sleep(5)
 
-efs.create_mount_target(
-    FileSystemId=efs_id,
-    SubnetId=subnet_id,
-    SecurityGroups=[security_group]
-)
+os.system(f'ssh -o StrictHostKeyChecking=no -i {clave} ec2-user@{public_ip} "sudo yum install -y amazon-efs-utils"')
+os.system(f'ssh -i {clave} ec2-user@{public_ip} "sudo mkdir -p /mnt/efs"')
 
-time.sleep(20)
+os.system(f'ssh -i {clave} ec2-user@{public_ip} "sudo mount -t efs {efs_id}:/ /mnt/efs"')
 
-os.system(
-    f'ssh -o StrictHostKeyChecking=no -i {clave} ec2-user@{public_ip} '
-    f'"sudo yum install -y amazon-efs-utils"'
-)
+resultado = os.system(f'ssh -i {clave} ec2-user@{public_ip} "mount | grep efs"')
+if resultado != 0:
+    raise Exception("EFS no se pudo montar en la EC2")
 
-os.system(
-    f'ssh -i {clave} ec2-user@{public_ip} '
-    f'"sudo mkdir -p /mnt/efs"'
-)
+os.system(f'scp -i {clave} {txt} ec2-user@{public_ip}:/tmp/{txt}')
+os.system(f'ssh -i {clave} ec2-user@{public_ip} "sudo mv /tmp/{txt} /mnt/efs/{txt}"')
 
-os.system(
-    f'ssh -i {clave} ec2-user@{public_ip} '
-    f'"sudo mount -t efs {efs_id}:/ /mnt/efs"'
-)
-os.system(
-    f'scp -i {clave} {txt} ec2-user@{public_ip}:/tmp/{txt}'
-)
-
-os.system(
-    f'ssh -i {clave} ec2-user@{public_ip} '
-    f'"sudo mv /tmp/{txt} /mnt/efs/{txt}"'
-)
-
-print("Archivo copiado al EFS correctamente")
+print("Archivo copiado correctamente al EFS")
